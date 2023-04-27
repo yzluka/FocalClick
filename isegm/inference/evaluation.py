b24fb939a11899b78f4b9d8fed6c61e4efe34514
yzluka <lucas.yixin.zhang@outlook.com>
2023-04-27 00:43:11 -0400

    initial modification

--- isegm/inference/evaluation.py
+++ isegm/inference/evaluation.py
@@ -54,25 +54,18 @@ def Progressive_Merge(pred_mask, previous_mask, y, x):
     return progressive_mask
 
 
-def evaluate_sample(image, gt_mask, init_mask, predictor, max_iou_thr,
+def evaluate_sample(image, gt_mask, predictor, max_iou_thr,
                     pred_thr=0.49, min_clicks=1, max_clicks=20,
-                    sample_id=None, vis = True, save_dir = None, index = 0,  callback=None,
-                    progressive_mode = True,
-                    ):
+                    sample_id=None, index = 0,  callback=None,
+                    progressive_mode = True):
     clicker = Clicker(gt_mask=gt_mask)
     pred_mask = np.zeros_like(gt_mask)
     prev_mask = pred_mask
     ious_list = []
-
+    num_pm=999
+    
     with torch.no_grad():
         predictor.set_input_image(image)
-        if init_mask is not None:
-            predictor.set_prev_mask(init_mask)
-            pred_mask = init_mask
-            prev_mask = init_mask
-            num_pm = 0
-        else:
-            num_pm = 999
             
         for click_indx in range(max_clicks):
             vis_pred = prev_mask
@@ -97,26 +90,34 @@ def evaluate_sample(image, gt_mask, init_mask, predictor, max_iou_thr,
             if iou >= max_iou_thr and click_indx + 1 >= min_clicks:
                 break
 
-        if vis:
-            if predictor.focus_roi is not None:
-                focus_roi = predictor.focus_roi
-                global_roi = predictor.global_roi
-                clicks_list = clicker.get_clicks()
-                last_y, last_x = predictor.last_y, predictor.last_x
-                focus_refined = predictor.focus_refined
-                focus_coarse = predictor.focus_coarse
-
-                out_image, focus_image = vis_result_refine(image, pred_mask, gt_mask, init_mask, iou,click_indx+1,clicks_list,focus_roi, global_roi, vis_pred, last_y, last_x, focus_refined, focus_coarse)
-                cv2.imwrite(save_dir+str(index)+'.png', out_image)
-                cv2.imwrite(save_dir+str(index)+'_focus.png', focus_image)
-                
-            else:
-                clicks_list = clicker.get_clicks()
-                last_y, last_x = predictor.last_y, predictor.last_x
-                out_image = vis_result_base(image, pred_mask, gt_mask, init_mask, iou,click_indx+1,clicks_list, vis_pred, last_y, last_x)
-                cv2.imwrite(save_dir+str(index)+'.png', out_image)
         return clicker.clicks_list, np.array(ious_list, dtype=np.float32), pred_probs
 
+    
+def evaluate_sample_onepass(image, gt_mask, predictor, clicks, pred_thr=0.49, iterative=True):
+    clicker = Clicker()
+    pred_mask = np.zeros_like(gt_mask)
+    ious_list = []
+    
+    with torch.no_grad():
+        predictor.set_input_image(image)
+        
+    if iterative:
+        for click in clicks:
+            clicker.add_click(click)
+            pred_probs = predictor.get_prediction(clicker)
+            pred_mask = pred_probs > pred_thr
+            ious_list.append(utils.get_iou(gt_mask, pred_mask))
+    else:
+        for click in clicks:
+            clicker.add_click(click)
+            
+        pred_probs = predictor.get_prediction(clicker)
+        pred_mask = pred_probs > pred_thr
+        ious_list.append(utils.get_iou(gt_mask, pred_mask))
+       
+    
+
+    return pred_mask, pred_probs, ious_list
 
 
 
